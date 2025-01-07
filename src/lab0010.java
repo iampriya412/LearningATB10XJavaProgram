dc024a0edea6121443c63d1f253c772c69c7d7d5
iampriya412 <kumari04priya1993@gmail.com>
2025-01-07 18:05:27 +0530

    test

---
+++ src/lab0010.java
@@ -0,0 +1,14 @@
+public class lab0010 {
+    public static void main(String[] args) {
+        int i =6;
+        if(i%2!=0)
+        {
+            System.out.println("Even number");
+        }
+        else
+
+        {
+            System.out.println("Odd number");
+        }
+    }
+}
---
+++ src/lab007.java
@@ -0,0 +1,17 @@
+public class lab007 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++)
+        {
+            if (n%2==0)
+            {
+                System.out.println("even number");
+            }
+            else
+            {
+                System.out.println("odd number");
+            }
+        }
+        }
+    }
+
